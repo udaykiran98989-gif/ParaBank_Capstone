06b526166d9c20c6aa1610a898f732a0355e941f
udaykiran98989-gif <udaykiran98989@gmail.com>
2026-05-24 22:58:53 +0530

    Updated framework fixes and Jenkins integration

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
 
         features = "src/test/resources/features",
-        glue = {"stepdefinitions,hooks"},
+        glue = {"stepdefinitions","hooks"},
         monochrome = true,
         plugin = {
                 "pretty",
